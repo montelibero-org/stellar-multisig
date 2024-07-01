2ca5123f2c1a89435d66a1a9dd5580603fd72f47
Firas Boukhriss <162185577+firasbk7770@users.noreply.github.com>
2024-07-01 00:01:08 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    assetPrefix,
-    basePath,
-    output: "export", // Ensure this line is present
-    images: {
-        unoptimized: true,
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
