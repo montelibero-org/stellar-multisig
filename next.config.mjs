c20e876f589a5bf33109d73685797537f1762509
Firas Boukhriss <162185577+firasbk7770@users.noreply.github.com>
2024-07-01 05:22:15 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Add this line to enable static export
+};
 
 export default nextConfig;
