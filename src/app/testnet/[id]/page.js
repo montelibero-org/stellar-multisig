90b25cd0c59002a2d5ace1df55e8d9824c080931
FullStackDev001 <162185577+firasbk7770@users.noreply.github.com>
2024-07-02 00:25:15 -0700

    modified files for deploy

--- src/app/testnet/[id]/page.js
+++ src/app/testnet/[id]/page.js
@@ -2,13 +2,16 @@ import PublicNet from "./testnet";
 
 export async function generateStaticParams() {
     // Fetch or define the possible values for `id`
-    const ids = ["account1", "account2", "account3"]; // Example ids
-    return ids.map(id => ({ id }));
+
+    const pathname = window.location.pathname;
+    const accountId = pathname.substring(pathname.lastIndexOf("/") + 1);
+
+    return accountId;
 }
 
 const PublicPage = ({ params }) => {
-    const { id } = params;
-    return <PublicNet id={id} />;
+    const accountId = params;
+    return <PublicNet accountId={accountId} />;
 };
 
 export default PublicPage;
