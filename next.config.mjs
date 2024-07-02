d13bdc37044600dbbe4ce44066893891fa0d5b04
Firas Boukhriss <162185577+firasbk7770@users.noreply.github.com>
2024-07-01 23:53:04 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Add this line to enable static export
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
