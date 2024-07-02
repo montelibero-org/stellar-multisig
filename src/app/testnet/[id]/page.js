d1b2ce3376730b5cb581b7e58dbcbd302d36b691
FullStackDev001 <162185577+firasbk7770@users.noreply.github.com>
2024-07-02 10:37:34 -0700

    modified files for deploy

--- src/app/testnet/[id]/page.js
+++ src/app/testnet/[id]/page.js
@@ -1,14 +1,26 @@
-"use client";
+import PublicNet from "./testnet";
+import axios from 'axios';
 
-import MainLayout from "@/components/layouts";
-
-const PublicNet = ({ props }) => {
-
-    return (
-        <MainLayout>
-
-        </MainLayout>
-    );
+// Fetching account IDs using Stellar Expert API
+const getAccountIds = async () => {
+  const apiStellarURI = "https://api.stellar.expert/explorer/directory?limit=20";
+  try {
+    const response = await axios.get(apiStellarURI);
+    const accounts = response.data._embedded.records;
+    return accounts.map(account => ({ id: account.address }));
+  } catch (error) {
+    console.error('Error fetching accounts from Stellar Expert:', error);
+    return [];
+  }
 };
 
-export default PublicNet;
+export async function generateStaticParams() {
+  const accounts = await getAccountIds();
+  return accounts.map((account) => ({
+    params: { id: account.id },
+  }));
+}
+
+export default function Page({ params }) {
+  return <PublicNet params={params} />;
+}
