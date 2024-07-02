d6dde58af2f913f280802766f8d994967059abbd
Firas Boukhriss <162185577+firasbk7770@users.noreply.github.com>
2024-07-02 11:41:31 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,8 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         disableStaticImages: true, // Disable Image Optimization API for export mode
     },
-    trailingSlash: false,   // Ensure that each exported page has a trailing slash
 };
 
 export default nextConfig;
