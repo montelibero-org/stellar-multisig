a341c20298a1daf58574d203124c87f7106ad730
FullStackDev001 <162185577+firasbk7770@users.noreply.github.com>
2024-07-02 21:28:16 -0700

    update config file for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        disableStaticImages: true, // Disable Image Optimization API for export mode
-    }
 };
 
 export default nextConfig;
