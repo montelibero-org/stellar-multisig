1257be54f41ede67cea4e226a704ff5a909a7f88
FullStackDev001 <162185577+firasbk7770@users.noreply.github.com>
2024-07-02 19:21:07 -0700

    modified

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         disableStaticImages: true, // Disable Image Optimization API for export mode
     },
+    output: 'export'
 };
 
 export default nextConfig;
