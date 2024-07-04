78bfbae03a71a896a8f433aa65ce4dcbd7001799
Vitaliy Shevchenko <grandmotivator@gmail.com>
2024-07-04 14:20:07 +0700

    deploy fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    distDir: 'out',
-    assetPrefix,
-    basePath,
+    output: "export",
     images: {
         unoptimized: true,
     },
