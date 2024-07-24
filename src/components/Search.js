81cb8852ee05a36197d6c4a76e7ce058a72e2294
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-07-24 13:46:22 +0300

    bug fix search

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { Search } from "lucide-react";
-import StellarSdk from 'stellar-sdk';
+import StellarSdk from "stellar-sdk";
 import { useRouter } from "next/navigation";
 import { usePublic } from "@/context/net";
 import {
@@ -16,9 +16,7 @@ const SearchBar = () => {
     const [search, setSearch] = useState("");
     const [net, setNet] = usePublic();
     const router = useRouter();
-    const [errorvalid, setErrorvalid] = useState(null)
-
-
+    const [errorvalid, setErrorvalid] = useState(null);
     const [exists, setExists] = useState(null);
 
     if (exists === true) {
@@ -27,15 +25,15 @@ const SearchBar = () => {
     }
 
     const changeHandler = (e) => {
-        setErrorvalid(null)
+        setErrorvalid(null);
         setSearch(e.currentTarget.value);
     };
-
+    
     const checkAccount = async () => {
         const serverUrl =
-            net === 'testnet'
-                ? 'https://horizon-testnet.stellar.org'
-                : 'https://horizon.stellar.org';
+            net === "testnet"
+                ? "https://horizon-testnet.stellar.org"
+                : "https://horizon.stellar.org";
         const server = new StellarSdk.Server(serverUrl);
 
         try {
@@ -46,8 +44,9 @@ const SearchBar = () => {
         } catch (e) {
             if (e instanceof StellarSdk.NotFoundError) {
                 setExists(false);
-                setErrorvalid('Error: Account does not exist on the network. Make sure that you copied account address correctly and there was at least one payment to this address.')
-
+                setErrorvalid(
+                    "Error: Account does not exist on the network. Make sure that you copied account address correctly and there was at least one payment to this address."
+                );
             } else {
                 console.error(e);
             }
@@ -60,14 +59,13 @@ const SearchBar = () => {
         }
 
         if (StellarSdk.StrKey.isValidEd25519PublicKey(search)) {
-            console.log('true')
+            console.log("true");
             checkAccount();
-
         } else {
             setTimeout(() => {
                 setExists(false);
             }, 2000);
-            setErrorvalid(`Not found at ${search}`)
+            setErrorvalid(`Not found at ${search}`);
         }
 
         // router.push(`/search/${search}`);
@@ -89,16 +87,24 @@ const SearchBar = () => {
     //     handle();
     // })
 
-
     return (
         <>
-        {errorvalid ? (
-                    <div className={`search ${exists === false ? 'error' : ''} container narrow`} style={{ padding: '20px' }} >
-                        <h2 className="text-overflow">Search results for {search}</h2>
-                        {exists === null && <p>Loading...</p>}
-                        {exists === false && <span>{errorvalid}</span>}
-                    </div>
-                ) : ('')}
+            {errorvalid ? (
+                <div
+                    className={`search ${
+                        exists === false ? "error" : ""
+                    } container narrow`}
+                    style={{ padding: "20px" }}
+                >
+                    <h2 className="text-overflow">
+                        Search results for {search}
+                    </h2>
+                    {exists === null && <p>Loading...</p>}
+                    {exists === false && <span>{errorvalid}</span>}
+                </div>
+            ) : (
+                ""
+            )}
             <div className="search-wrapper">
                 <div
                     style={{
@@ -116,13 +122,11 @@ const SearchBar = () => {
                 <input
                     className="search"
                     value={search}
-                    onKeyDown={keyDownHandler}
-                    onChange={changeHandler}
+                    onKeyDown={(e) => keyDownHandler(e)}
+                    onChange={(e) => changeHandler(e)}
                     placeholder="Paste an account address here"
                 />
             </div>
-
-
         </>
     );
 };
