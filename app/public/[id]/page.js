a62757aa63995f03dda2101a2bf2d3bf4003b30f
Vitaliy Shevchenko <grandmotivator@gmail.com>
2024-08-01 11:22:51 +0700

    Revert "routes test"
    This reverts commit e36153ae0402160db2c197056f572622f413dfa4.

--- app/public/[id]/page.js
+++ app/public/[id]/page.js
@@ -27,7 +27,6 @@ export async function generateStaticParams() {
 
 // Page component
 export default function Page({ params }) {
-    console.log("Received key:", params.id);
     if (!StellarSdk.StrKey.isValidEd25519PublicKey(params.id)) {
         // Handle case where params are not found or invalid
         return (
@@ -50,4 +49,4 @@ export default function Page({ params }) {
     }
     return <PublicNet params={params} />;
 }
-//
+// 
