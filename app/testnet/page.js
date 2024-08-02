2ed2842d370b46067de1740254dabc1a14d51d82
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-08-02 13:27:20 +0300

    fix bug white theme

--- app/testnet/page.js
+++ app/testnet/page.js
@@ -1,9 +1,8 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import SearchBar from "@/components/Search";
 import MainLayout from "@/components/layouts";
 import { usePublic } from "@/context/net";
-import { useRouter } from "next/navigation";
 export default function Home() {
     const [net, setNet] = usePublic();
     return (
--- src/components/layouts/header/index.js
+++ src/components/layouts/header/index.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { usePublic } from "@/context/net";
 
-const Header = () => {
+const Header = ({ theme, setTheme }) => {
     const router = useRouter();
     const [net, setNet] = usePublic();
     const [isOpen, setIsOpen] = useState(false);
@@ -16,18 +16,21 @@ const Header = () => {
     useEffect(() => {
         // Function to handle click outside dropdown
         function handleClickOutside(event) {
-            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target)
+            ) {
                 setIsOpen(false); // Close dropdown if clicked outside
             }
         }
 
         // Bind the event listener
-        document.addEventListener('mousedown', handleClickOutside);
+        document.addEventListener("mousedown", handleClickOutside);
         return () => {
             // Unbind the event listener on cleanup
-            document.removeEventListener('mousedown', handleClickOutside);
+            document.removeEventListener("mousedown", handleClickOutside);
         };
-    }, []);
+    }, [theme, setTheme]);
 
     const toggleDropdown = () => setIsOpen(!isOpen);
 
@@ -39,14 +42,19 @@ const Header = () => {
         const currentPath = window.location.pathname;
         console.log(currentPath);
         // Check if the current path contains 'public' or 'testnet'
-        if ( currentPath == "/public" ||currentPath == "/testnet" ) {
+        if (currentPath == "/public" || currentPath == "/testnet") {
             const newPath = `/${network}`;
 
             // Navigate to the new path
             router.push(newPath);
-        } else if (currentPath.includes('/public/') || currentPath.includes('/testnet/')) {
+        } else if (
+            currentPath.includes("/public/") ||
+            currentPath.includes("/testnet/")
+        ) {
             // Construct the new path with updated network segment
-            const newPath = `/${network}${currentPath.substring(currentPath.indexOf('/', 1))}`;
+            const newPath = `/${network}${currentPath.substring(
+                currentPath.indexOf("/", 1)
+            )}`;
 
             // Navigate to the new path
             router.push(newPath);
@@ -75,7 +83,13 @@ const Header = () => {
                 </Link>
                 <div className="nav-menu-dropdown false">
                     <div className="main-menu top-menu-block">
-                        <Link href={"/"+net+"/assets"}>Assets</Link>
+                        <Link href={"/" + net + "/assets"}>
+                            <p style={{
+                                marginTop: "-1.5px"
+                            }} className={theme === "day" ? "text-color" : undefined}>
+                                Assets
+                            </p>
+                        </Link>
                     </div>
                     <div
                         className="top-menu-block right"
@@ -84,14 +98,87 @@ const Header = () => {
                         }}
                     >
                         <div className="dropdown" ref={dropdownRef}>
-                            <div className="dropdown-header" onClick={toggleDropdown}>
-                                Network <span className="dropdown-selected">{net}</span>
-                                <span className={isOpen ? "dd-toggle visible" : "dd-toggle"} ></span>
+                            <div
+                                className={
+                                    theme === "day"
+                                        ? "dropdown-header-light"
+                                        : "dropdown-header"
+                                }
+                                onClick={toggleDropdown}
+                            >
+                                <span
+                                    className={
+                                        theme === "day"
+                                            ? "network-text-light"
+                                            : "network-text"
+                                    }
+                                >
+                                    Network{" "}
+                                </span>
+                                {theme !== "day" ? (
+                                    <span className={`dropdown-selected`}>
+                                        {net}
+                                    </span>
+                                ) : (
+                                    <span
+                                        style={{
+                                            marginInline: "5px",
+                                            color: "#666",
+                                        }}
+                                    >
+                                        {net}
+                                    </span>
+                                )}
+                                <span
+                                    className={`
+                                        ${
+                                            isOpen
+                                                ? theme === "day" ? "dd-toggle-light visible" : "dd-toggle visible"
+                                                : theme === "day" ? "dd-toggle-light" : "dd-toggle"
+                                        }
+                                    `}
+                                ></span>
                             </div>
                             {isOpen && (
-                                <div className="dropdown-menu">
-                                    <div className={`dropdown-item ${net === 'public' ? 'selected' : ''}`} onClick={() => handleSelect('public')}>public</div>
-                                    <div className={`dropdown-item ${net === 'testnet' ? 'selected' : ''}`} onClick={() => handleSelect('testnet')}>testnet</div>
+                                <div
+                                    className={
+                                        theme === "day"
+                                            ? "dropdown-menu-light"
+                                            : "dropdown-menu"
+                                    }
+                                >
+                                    <div
+                                        className={
+                                            theme === "night"
+                                                ? `dropdown-item ${
+                                                      net === "testnet" &&
+                                                      "selected"
+                                                  }`
+                                                : `dropdown-item-light ${
+                                                      net === "testnet" &&
+                                                      "selected"
+                                                  }`
+                                        }
+                                        onClick={() => handleSelect("public")}
+                                    >
+                                        public
+                                    </div>
+                                    <div
+                                        className={
+                                            theme === "night"
+                                                ? `dropdown-item ${
+                                                      net === "public" &&
+                                                      "selected"
+                                                  }`
+                                                : `dropdown-item-light ${
+                                                      net === "public" &&
+                                                      "selected"
+                                                  }`
+                                        }
+                                        onClick={() => handleSelect("testnet")}
+                                    >
+                                        testnet
+                                    </div>
                                 </div>
                             )}
                         </div>
