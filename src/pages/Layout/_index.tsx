ba4414f9acabc7e2ba0c3a38851d393b3fc92b3a
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-08-11 15:54:34 +0300

    fix ReferenceError: window is not defined

--- src/pages/Layout/_index.tsx
+++ src/pages/Layout/_index.tsx
@@ -1,47 +1,63 @@
-"use client";
-
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { useStore } from "@/features/store";
 import { Footer, Header } from "@/widgets";
 import { useShallow } from "zustand/react/shallow";
 
 type Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 const PageLayout: FC<Props> = ({ children }) => {
-    const { theme, setTheme, setNet } = useStore(
-        useShallow((state) => ({
-            theme: state.theme,
-            setTheme: state.setTheme,
-            setNet: state.setNet,
-        }))
-    );
+  const [isWindowDefined, setIsWindowDefined] = useState(false);
 
-    useEffect(() => {
-        if (localStorage.getItem("theme")) setTheme(localStorage.getItem("theme")!);
-        if (localStorage.getItem("net")) setNet(localStorage.getItem("net")!);
-    }, [setTheme, setNet]);
+  const { theme, setTheme, setNet } = useStore(
+    useShallow((state) => ({
+      theme: state.theme,
+      setTheme: state.setTheme,
+      setNet: state.setNet,
+    }))
+  );
 
-    const themeLS = window.localStorage.getItem("theme");
+  useEffect(() => {
+    setIsWindowDefined(typeof window !== 'undefined');
+    if (isWindowDefined) {
+      if (localStorage.getItem("theme")) setTheme(localStorage.getItem("theme")!);
+      if (localStorage.getItem("net")) setNet(localStorage.getItem("net")!);
+    }
+  }, [setTheme, setNet, isWindowDefined]);
 
-    return (
-        <html lang="en" data-theme={!theme || themeLS}>
-            <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta name="commit-hash" content={process.env.NEXT_PUBLIC_COMMIT_HASH || ""} />
-                <title>Stellar Multisig</title>
-            </head>
-            <body>
-                <main className="flex min-h-screen flex-col">
-                    <hr className="blue-ribbon" />
-                    <Header />
-                    {children}
-                    <Footer />
-                </main>
-            </body>
-        </html>
-    );
+  const themeLS: string = isWindowDefined ? String(window.localStorage.getItem("theme")) : '';
+
+  if (!isWindowDefined) {
+    return <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="commit-hash" content={process.env.NEXT_PUBLIC_COMMIT_HASH || ""} />
+        <title>Stellar Multisig</title>
+        </head>
+        <body>
+
+        </body>
+    </html>;
+  }
+
+  return (
+    <html lang="en" data-theme={!theme || themeLS}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="commit-hash" content={process.env.NEXT_PUBLIC_COMMIT_HASH || ""} />
+        <title>Stellar Multisig</title>
+      </head>
+      <body>
+        <main className="flex min-h-screen flex-col">
+          <hr className="blue-ribbon" />
+          <Header />
+          {children}
+          <Footer />
+        </main>
+      </body>
+    </html>
+  );
 };
 
 export default PageLayout;
