68ea302d7c2aa4a75cc717337a631a511f7d94ed
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-08-19 08:12:51 +0300

    fix error 2

--- src/pages/public/account/publicnet.tsx
+++ src/pages/public/account/publicnet.tsx
@@ -164,7 +164,6 @@ const PublicNet: FC<Props> = ({ id }) => {
         setIsVisibleHomeDomainInfo(false);
       }
     } else {
-      console.error("No TOML info available");
       setIsVisibleHomeDomainInfo(false);
     }
   }, [information.tomlInfo, id]);
