61d2fc75a85776cc852331c25870b83db7fbb978
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-08-23 09:37:36 +0300

    Added cached data in information of account

---
+++ src/shared/configs/index.ts
@@ -0,0 +1,4 @@
+export {default as trustedMtlAssets} from './trusted-mtl-assets.json';
+export {default as ignoredHomeDomains} from './ignored-home-domains.json';
+export {default as dataKeys} from './data-keys.json';
+export {default as cacheConfig} from './cache-config.json'
